846c1ef30003b449b9abe077164f59a0a79b4285
SelfRidicule <SelfRidicule@gmail.com>
2022-04-07 14:51:20 +0800

    init

---
+++ src/main/java/com/self/cloud/SelfCloudAlibabaApplication.java
@@ -0,0 +1,14 @@
+package com.self.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfCloudAlibabaApplication {
+
+	// 启动类
+	public static void main(String[] args) {
+		SpringApplication.run(SelfCloudAlibabaApplication.class, args);
+	}
+
+}
