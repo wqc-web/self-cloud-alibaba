f0ee22cce6497f052181997eef1173ca1ad67e8a
SelfRidicule <selfridicule@gmail.com>
2022-04-09 00:40:36 +0800

    描述：修改

--- src/main/java/com/self/cloud/domain/entity/User.java
+++ src/main/java/com/self/cloud/domain/entity/User.java
@@ -1,10 +1,15 @@
 package com.self.cloud.domain.entity;
 
-import javax.persistence.*;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * 表名：t_user
-*/
+ */
+@Data
 @Table(name = "t_user")
 public class User {
     @Id
@@ -18,73 +23,7 @@ public class User {
 
     private String source;
 
-    /**
-     * @return id
-     */
-    public Integer getId() {
-        return id;
-    }
+    @Column(name = "wx_id")
+    private String wxId;
 
-    /**
-     * @param id
-     */
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    /**
-     * @return name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * @param password
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * @return phone
-     */
-    public String getPhone() {
-        return phone;
-    }
-
-    /**
-     * @param phone
-     */
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    /**
-     * @return source
-     */
-    public String getSource() {
-        return source;
-    }
-
-    /**
-     * @param source
-     */
-    public void setSource(String source) {
-        this.source = source;
-    }
 }
