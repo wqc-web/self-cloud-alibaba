0f4ff43d94b9d8ceaaa77c00c017e038aa51983b
SelfRidicule <SelfRidicule@gmail.com>
2022-04-10 15:03:27 +0800

    描述：nacos 服务发现、服务配置、服务元数据及流量管理

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Mapper 使用通用Mapper简化不必要sql，例如CRUD
 mybatis-generator-maven-plugin 自动生成实体类和Mapper文件
 赋值对象和对象的属性值使用spring提供的BeanUtils.copyProperties(Object source, Object target) 
 整合spring-cloud、spring-cloud-alibaba版本管理
+nacos 服务发现、服务配置、服务元数据及流量管理
 ```
--- src/main/java/com/self/cloud/controller/TestController.java
+++ src/main/java/com/self/cloud/controller/TestController.java
@@ -4,10 +4,14 @@ import com.self.cloud.dao.UserMapper;
 import com.self.cloud.domain.dto.UserDto;
 import com.self.cloud.domain.entity.User;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,6 +19,8 @@ public class TestController {
 
     @Resource
     UserMapper userMapper;
+    @Resource
+    DiscoveryClient discoveryClient;
 
     @GetMapping("/test")
     public Object test(){
@@ -24,6 +30,17 @@ public class TestController {
         BeanUtils.copyProperties(user , userDto);
         userDto.setOther("其他");
         return userDto;
+    }
+
+    @GetMapping("/nacosServiceList")
+    public List<List> nacosServiceList(){
+        List<List> dataList = new ArrayList<>();
+        List<String> serviceIdList = discoveryClient.getServices();
+        for (String serviceId : serviceIdList) {
+            List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+            dataList.add(instances);
+        }
+        return dataList;
     }
 
 }
